7fff1cf6a6caff530b77b304d6e504391ee556a3
gvl610 <giangvinhloc610@gmail.com>
2024-06-29 12:02:26 +0700

    Drivebase tunning completed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Roadrunner/drive/DriveConstants.java
@@ -44,9 +44,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 1.77; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.3; // in
+    public static double WHEEL_RADIUS = 1.77165; // in
+    public static double GEAR_RATIO = 1.02; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 12.07; // ins
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -54,9 +54,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.0455;
+    public static double kA = 0.00026;
+    public static double kStatic = 0.0387;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -65,10 +65,10 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_VEL = 15;
+    public static double MAX_ACCEL = 15;
+    public static double MAX_ANG_VEL = 143.80002699656978;
+    public static double MAX_ANG_ACCEL = 2.509783935546875;
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.

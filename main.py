29457f6de8c135c2dbdf8c238d0bb90c29018e9c
phm-tuyenn <phamthanhtuyen2k8@gmail.com>
2024-06-29 16:45:07 +0700

    shift the fucking center of the map

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ df = pd.read_csv("point.csv")
 img = np.ones((1000,1000,3), np.uint8) * 255
 
 for point in df.values:
-    cv2.circle(img, (point[0], point[1]), 1, (0, 0, 255), 3)
+    cv2.circle(img, (point[0] + 500, point[1] + 500), 1, (0, 0, 255), 3)
 
 cv2.imshow("frame", img)
 
